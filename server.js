a9750091dab161f424ad3e2c77b4892a597e5fa9
jimmy <rucker>
2019-06-25 02:43:19 -0500

    Mvc
    Mvc setup , setup a landing , pet home page , user page and favorite route. These routes can be changed to what ever we need going foward

--- server.js
+++ server.js
@@ -1,15 +1,60 @@
 require('dotenv').config();
 
 var express = require('express');
+var path = require('path');
+var createError = require('http-errors');
 var app = express();
 var port = process.env.PORT || 9000;
 var config = require('./config/config');
+
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+// Route Setup
+var indexLanding = require('./routes/landing');
+var indexHome = require('./routes/home');
+var usersRouter = require('./routes/users');
+var usersFavorites = require('./routes/favorites');
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.static(__dirname + '/views')); // you should change this to be wherever your html files are
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
+
+
+
+// Route Paths
+app.use('/', indexLanding);
+app.use('/home', indexHome);
+app.use('/users', usersRouter);
+app.use('/favorites', usersFavorites);
+
 app.listen(port);
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
+  });
+  
+  // error handler
+  app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
+  
+  module.exports = app;
+
 
 //@TODO Delete below after you verify the the app is working
 app.route('/').get(function(request, response) {
